df12b44679b442a80ae5a11afed4d5813b5f33a8
Andrea Russo <andrea.russo@cmcc.it>
2018-01-22 18:50:30 +0100

    Update the state file config for remote s3 backend.

--- config.tf
+++ config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "cmcc-terraform-state"
-    key            = "eucp-xwiki/terraform.tfstate"
+    bucket         = "cmcc-eucp-xwiki-remote-state"
+    key            = "global/s3/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "cmcc_terraform_lock"
   }
