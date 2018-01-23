22b793cccdcb38b4523f6afd0121f27d3303a6fa
Andrea Russo <andrea.russo@cmcc.it>
2018-01-23 14:44:03 +0100

    Updated.

--- services/xwiki/interface.tf
+++ services/xwiki/interface.tf
@@ -7,3 +7,8 @@ variable "service" {
   description = "The service name this resources are for"
   default     = "cmcc-eucp-xwiki"
 }
+
+output "public_ip" {
+  description = "The XWiki instance public ip"
+  value       = "${module.ec2_cluster.public_ip}"
+}
--- services/xwiki/main.tf
+++ services/xwiki/main.tf
@@ -12,18 +12,6 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-module "security_group" {
-  source = "github.com/rastandy/terraform-aws-security-group?ref=1.13.0"
-
-  name        = "${var.service}-security-group"
-  description = "Security group for ${var.service} usage with EC2 instance"
-  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["ssh-tcp", "http-8080-tcp", "http-80-tcp", "all-icmp"]
-  egress_rules        = ["all-all"]
-}
-
 data "aws_ami" "centos_7" {
   most_recent = true
 
@@ -51,18 +39,32 @@ resource "aws_key_pair" "keypair" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCom3uwiMB266XmVvBu9wHaQf8VQyPDhBBiwOHBEiDU1mo258dqmRaGNslHZ09pbvLhqdkJL9JveHbJJq4vHSdjPQCxXvp2O+4UcokFewNkKOz+IssLWyYV+3HmFWy4n6KMkpEsGLu6JW8z3j1xMibu8Z7JquMC8Im+TenjpEfhaED5Zuo8I1E7QgmNnoX62dD3hrz70Je1t7yiSuOVoyqEX4bylvasMQOLamKfcSahLfr17o8hgHNcLnXwTc//rgK1vukVBpYIjyUmXpiAlKqPRRYTB6f8tAeOn3Mt4Be4+9pQG5RqCy0SzaGkDqLaxtwZ2eZ/AJYK8PzXuUyQ2lqZ arusso@Andrea-Mac.local"
 }
 
+module "security_group" {
+  source = "github.com/rastandy/terraform-aws-security-group?ref=1.13.0"
+
+  name        = "${var.service}-security-group"
+  description = "Security group for ${var.service} usage with EC2 instance"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["ssh-tcp", "http-8080-tcp", "http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+}
+
 module "ec2_cluster" {
   source = "github.com/rastandy/terraform-aws-ec2-instance?ref=v1.2.0"
 
   name  = "xwiki-instance-${terraform.workspace}"
   count = 1
 
-  ami                    = "${data.aws_ami.centos_7.image_id}"
-  instance_type          = "t2.micro"
-  key_name               = "${aws_key_pair.keypair.key_name}"
-  monitoring             = false
-  vpc_security_group_ids = ["${module.security_group.this_security_group_id}"]
-  subnet_id              = "${data.terraform_remote_state.vpc.public_subnets.0}"
+  ami                         = "${data.aws_ami.centos_7.image_id}"
+  instance_type               = "t2.micro"
+  key_name                    = "${aws_key_pair.keypair.key_name}"
+  monitoring                  = false
+  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
+  subnet_id                   = "${data.terraform_remote_state.vpc.public_subnets.0}"
+  associate_public_ip_address = true
+  ebs_optimized               = true
 
   tags = {
     Environment = "${terraform.workspace}"
