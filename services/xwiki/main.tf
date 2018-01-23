6f922c7bf874cffd9743798cb30b5bdbcf34641e
Andrea Russo <andrea.russo@cmcc.it>
2018-01-23 12:31:16 +0100

    Updated the Centos 7 ami search parameters.

--- services/xwiki/main.tf
+++ services/xwiki/main.tf
@@ -27,11 +27,13 @@ module "security_group" {
 data "aws_ami" "centos_7" {
   most_recent = true
 
+  owners = ["679593333241"]
+
   filter {
     name = "name"
 
     values = [
-      "Centos*-x86_64-*",
+      "CentOS Linux 7 x86_64 HVM EBS*",
     ]
   }
 
@@ -39,9 +41,14 @@ data "aws_ami" "centos_7" {
     name = "owner-alias"
 
     values = [
-      "Centos",
+      "aws-marketplace",
     ]
   }
+}
+
+resource "aws_key_pair" "keypair" {
+  key_name   = "${var.service}-key-pair"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCom3uwiMB266XmVvBu9wHaQf8VQyPDhBBiwOHBEiDU1mo258dqmRaGNslHZ09pbvLhqdkJL9JveHbJJq4vHSdjPQCxXvp2O+4UcokFewNkKOz+IssLWyYV+3HmFWy4n6KMkpEsGLu6JW8z3j1xMibu8Z7JquMC8Im+TenjpEfhaED5Zuo8I1E7QgmNnoX62dD3hrz70Je1t7yiSuOVoyqEX4bylvasMQOLamKfcSahLfr17o8hgHNcLnXwTc//rgK1vukVBpYIjyUmXpiAlKqPRRYTB6f8tAeOn3Mt4Be4+9pQG5RqCy0SzaGkDqLaxtwZ2eZ/AJYK8PzXuUyQ2lqZ arusso@Andrea-Mac.local"
 }
 
 module "ec2_cluster" {
@@ -50,9 +57,9 @@ module "ec2_cluster" {
   name  = "xwiki-instance-${terraform.workspace}"
   count = 1
 
-  ami                    = "ami-7cbc6e13"
+  ami                    = "${data.aws_ami.centos_7.image_id}"
   instance_type          = "t2.micro"
-  key_name               = "eucp-xwiki"
+  key_name               = "${aws_key_pair.keypair.key_name}"
   monitoring             = true
   vpc_security_group_ids = ["${module.security_group.this_security_group_id}"]
   subnet_id              = "${data.terraform_remote_state.vpc.public_subnets.0}"
