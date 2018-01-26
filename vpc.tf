8bb7301b690e0b0ea3dd34e6f0992d185605fe72
Andrea Russo <andrea.russo@cmcc.it>
2018-01-26 08:05:18 +0100

    Use the aws_availability_zones data source.

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,7 @@
+
+# Availabilty zones in the configured AWS region
+data "aws_availability_zones" "azs" {}
+
 module "vpc" {
   source = "github.com/rastandy/terraform-aws-vpc?ref=v1.17.0"
 
@@ -5,7 +9,7 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
 
-  azs              = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  azs              = ["${data.aws_availability_zones.azs.names}"]
   private_subnets  = ["10.0.1.0/24"]
   public_subnets   = ["10.0.101.0/24"]
   database_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
