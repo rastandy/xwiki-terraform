e8a0cd39b09b571bf5ef36b850983129776c3229
Andrea Russo <andrea.russo@cmcc.it>
2018-02-03 12:40:23 +0100

    Ansible templates and provision in a file apart.

---
+++ ansible.tf
@@ -0,0 +1,40 @@
+data "template_file" "host_vars_template" {
+  template = "${file("${path.module}/templates/host_vars.yml")}"
+
+  # depends_on = ["module.db"]
+
+  vars {
+    db_hostname = "${module.db.this_db_instance_address}"
+  }
+}
+
+resource "null_resource" "host_vars" {
+  triggers {
+    template_rendered = "${data.template_file.host_vars_template.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${data.template_file.host_vars_template.rendered}' > ansible/host_vars/${var.servername}"
+  }
+}
+
+data "template_file" "inventory_template" {
+  template = "${file("${path.module}/templates/inventory")}"
+
+  # depends_on = ["aws_eip.xwiki_eip"]
+
+  vars {
+    servername = "${var.servername}"
+    host_ip    = "${aws_eip.xwiki_eip.public_ip}"
+  }
+}
+
+resource "null_resource" "inventory" {
+  triggers {
+    template_rendered = "${data.template_file.inventory_template.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${data.template_file.inventory_template.rendered}' > ansible/inventory"
+  }
+}
